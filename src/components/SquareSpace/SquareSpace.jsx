4a257ea4adbb1184bf0b853446df63866f20530b
Alanna Celentano <celentanoad91@gmail.com>
2020-07-11 12:37:49 -0400

    update monster styling

--- src/components/SquareSpace/SquareSpace.jsx
+++ src/components/SquareSpace/SquareSpace.jsx
@@ -2,12 +2,19 @@ import React from 'react';
 import styles from './SquareSpace.module.css';
 
 const SquareSpace = (props) => {
+
+    let randomMonster = Math.floor(Math.random() * 10);
+    let randomTreasure = Math.floor(Math.random() * 10);
+
     return ( 
         <>
          <div className={props.playerLocation ? styles.Player 
-         : styles.SquareSpace}>
+         : styles.SquareSpace}
+          style={{backgroundColor: props.type === "Monster" ? "brown" : "blue"}}
+         >
 
-         {props.zone === 1 ? <p>Treasure</p> : <p>M</p>}
+         {/* {props.type === randomMonster ? <p className={styles.Monster}>M</p> : <div></div>} */}
+         {props.space === randomTreasure ? <p>T</p> : <p></p>}
          </div>
          
 
