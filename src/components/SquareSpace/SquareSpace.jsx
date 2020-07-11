9fdd566aa5c09463cd261e598ba31756dd9c6d76
Alanna Celentano <celentanoad91@gmail.com>
2020-07-11 08:37:09 -0400

    fix playerLocation props

--- src/components/SquareSpace/SquareSpace.jsx
+++ src/components/SquareSpace/SquareSpace.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import styles from './SquareSpace.module.css';
 
-const SquareSpace = () => {
+const SquareSpace = (props) => {
     return ( 
         <>
-         <div className={styles.SquareSpace} />
+         <div className={props.playerLocation ? styles.Player : styles.SquareSpace}
+         />
+         
         </>
      );
 }
