c7e8a073900d56d96e0b5c0720b02100548a237f
Skylar Wong <li.q.wong@gmail.com>
2020-07-12 11:47:04 -0500

    create basic list of cards

---
+++ src/card.js
@@ -0,0 +1,81 @@
+export const cards = [
+    ////// attack cards
+    {
+        name: 'Dagger',
+        type: 'Attack',
+        points: 1,
+    },
+    {
+        name:'Sword',
+        type: 'Attack',
+        points: 2
+    },
+    {
+        name: 'Bow and Arrows',
+        type: 'Attack',
+        points: 2
+    },
+    {
+        name: 'Spell of Fireballs',
+        type: 'Attack',
+        points: 3
+    },
+    {
+        name: 'Swarm of bees',
+        type: 'Attack',
+        points: 2
+    },
+    {
+        name: 'Bag of Big Rocks',
+        type: 'Attack',
+        points: 1
+    },
+    {
+        name: 'Mjolnir',
+        type: 'Attack',
+        points: 5
+    },
+    {
+        name: 'Catapult of Frozen Dung',
+        type: 'Attack',
+        points: 4
+    },
+    ///// defense cards
+    {
+        name:'Wooden Shield',
+        type: 'Defense',
+        points: 1,
+    },
+    {
+        name: 'Chainmail Body Armor',
+        type: 'Defense',
+        points: 2
+    },
+    {
+        name: 'Vibranium Shield',
+        type: 'Defense',
+        points: 3
+    },
+    /// healing cards
+    {
+        name: 'Fuzzy Berries',
+        type: 'Healing',
+        points: 1
+    },
+    {
+        name: 'Sparkling Blue Potion',
+        type: 'Healing',
+        points: 2
+    },
+    {
+        name: 'Hot Chocolate',
+        type: 'Healing',
+        points: 2
+    },
+    {
+        name: 'Barrel of Mead',
+        type: 'Healing',
+        points: 3
+    }
+
+]
