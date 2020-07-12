c0fbbc2c271d18903cc6bba48f0dcf1b9fd6e8fd
Alan McCracken <acmccracken@gmail.com>
2020-07-12 10:45:33 -0500

    added bosses

---
+++ src/boss.js
@@ -0,0 +1,83 @@
+export const bosses = {
+    0: {
+        name: 'Parasitic Ice Worm',
+        zone: 1,
+        currentHealth: 7,
+        maxHealth: 7,
+        attack: 3,
+        defense: 6
+    },
+    1: {
+        name: 'Banshee',
+        zone: 1,
+        currentHealth: 4,
+        maxHealth: 4,
+        attack: 7,
+        defense: 3
+    },
+    2: {
+        name: 'Goblin King',
+        zone: 2,
+        currentHealth: 7,
+        maxHealth: 9,
+        attack: 6,
+        defense: 5
+    },
+    3: {
+        name: 'Vampire',
+        zone: 2,
+        currentHealth: 4,
+        maxHealth: 10,
+        attack: 4,
+        defense: 7
+    },
+    4: {
+        name: 'Cyclops',
+        zone: 3,
+        currentHealth: 14,
+        maxHealth: 14,
+        attack: 7,
+        defense: 4
+    },
+    5: {
+        name: 'Hydra',
+        zone: 3,
+        currentHealth: 7,
+        maxHealth: 7,
+        attack: 11,
+        defense: 6
+    },
+    6: {
+        name: 'Manticore',
+        zone: 4,
+        currentHealth: 4,
+        maxHealth: 11,
+        attack: 9,
+        defense: 8
+    },
+    7: {
+        name: 'Cerberus',
+        zone: 4,
+        currentHealth: 4,
+        maxHealth: 15,
+        attack: 7,
+        defense: 9
+    },
+    8: {
+        name: 'Phoenix',
+        zone: 1,
+        currentHealth: 4,
+        maxHealth: 16,
+        attack: 9,
+        defense: 12
+    },
+    9: {
+        name: 'Dragon',
+        zone: 1,
+        currentHealth: 12,
+        maxHealth: 12,
+        attack: 17,
+        defense: 10
+    },
+    
+}
