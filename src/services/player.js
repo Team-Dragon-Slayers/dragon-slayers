836d0d32c6b3c57c82674252cc8f395aa0e525a3
Alan McCracken <acmccracken@gmail.com>
2020-07-12 13:45:53 -0500

    added cards to player hand

--- src/services/player.js
+++ src/services/player.js
@@ -1,6 +1,32 @@
 export const player = {
     name: '',
-    deck: [],
+    deck: [
+        {
+            name: 'Dagger',
+            type: 'Attack',
+            points: 1,
+        },
+        {
+            name:'Sword',
+            type: 'Attack',
+            points: 2
+        },
+        {
+            name: 'Spell of Fireballs',
+            type: 'Attack',
+            points: 3
+        },
+        {
+            name:'Wooden Shield',
+            type: 'Defense',
+            points: 1,
+        },
+        {
+            name: 'Fuzzy Berries',
+            type: 'Healing',
+            points: 1
+        },
+    ],
     zone: 1,
     row: 1,
     space: 1,
